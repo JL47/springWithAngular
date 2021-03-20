58fce09baa4d35c71c6b7714c491edcc11c7aa34
JL47 <jorlopez@csumb.edu>
2021-03-20 10:17:34 -0700

    Spring and angular working individually

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+//import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+  result = '';
+
+
+
+
 }
+
